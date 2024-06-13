f6895644ff6887dfe83b6483388e8ca0ee88baa8
fahru-razi <henteu.aya.namian@gmail.com>
2024-06-13 14:44:37 +0700

    Serve Machine Learning

---
+++ READ.md
@@ -0,0 +1,7 @@
+1. Build model Linear Regression 
+2. Develop model then save model as a pickle file `regression.pkl`
+3. Create new Python virtual environment using this command `python -m venv env`
+4. Activate Python virtual environment `.\env\Scripts\activate`
+5. Install Python libraries `pip install -r requirements.txt`
+6. Run `python regression_endpoint.py`
+7. Open browser and go to localhost:5010/predict-advertising?sales=<total-sales>
---
+++ regression_endpoint.py
@@ -0,0 +1,30 @@
+import numpy as np
+from flask import Flask, request, jsonify
+import pickle
+import os
+
+app = Flask(__name__)
+
+# Create endpoint /predict-advertising
+
+
+@app.route('/predict-advertising', methods=['POST', 'GET'])
+def predict():
+    # get argument parameter from endpoint
+    data = request.args
+
+    with open("regression.pkl", 'rb') as file:
+        pk_model = pickle.load(file)
+
+    # Predict with regression model
+    prediction = pk_model.predict([[np.array(eval(data['sales']))]])
+    # Get first index
+    print(prediction)
+    
+    # Return prediction result
+    return f"Prediksinya adalah: {prediction[0][0]}"
+
+
+# Run endpoint server
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5010, debug=True)
